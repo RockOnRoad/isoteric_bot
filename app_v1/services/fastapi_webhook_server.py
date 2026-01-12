3ec442d795866b7fdcb5995b148f5cf335c3217e
RockOnRoad <iHushHost@gmail.com>
2026-01-12 17:55:41 +0300

    a little edit

--- app_v1/services/fastapi_webhook_server.py
+++ app_v1/services/fastapi_webhook_server.py
@@ -1,3 +1,4 @@
+import asyncio
 import uvicorn
 from fastapi import FastAPI
 from fastapi.requests import Request
@@ -14,9 +15,16 @@ app = FastAPI()
 async def webhook(request: Request):
     payload = await request.json()
     print(payload)
-    user_id = payload.get("object", {}).get("metadata", {}).get("user_id")
+    user_id = (
+        payload.get("event", {}).get("object", {}).get("metadata", {}).get("user_id")
+    )
     # Add your processing logic here
-    await bot.send_message(user_id, "Payment received!")
+    print(f"User ID: {user_id}")
+
+    # Не блокируем ответ телеграм-запросом, запускаем отправку в фоне
+    if user_id:
+        asyncio.create_task(bot.send_message(user_id, "Payment received!"))
+
     return JSONResponse({"message": "Webhook received successfully"}, status_code=200)
 
 
@@ -29,8 +37,8 @@ async def start_fastapi():
         host="0.0.0.0",
         port=8000,
         log_level="info",  # debug, info, warning, error, critical
-        ssl_keyfile="certs/key.pem",
-        ssl_certfile="certs/cert.pem",
+        # ssl_keyfile="certs/key.pem",
+        # ssl_certfile="certs/cert.pem",
     )
     server = uvicorn.Server(config)
     await server.serve()  # this is async and will block, so run in a task
