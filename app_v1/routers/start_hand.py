99f8aa259f7dc81ad4930f3cac8ebafc5e8b0dee
RockOnRoad <iHushHost@gmail.com>
2026-01-27 02:27:20 +0300

    fooling around at this point

--- app_v1/routers/start_hand.py
+++ app_v1/routers/start_hand.py
@@ -87,6 +87,9 @@ async def handle_start_main(
                 try:
                     await update.message.edit_text(text, reply_markup=kbd.markup)
                 except TelegramBadRequest as e:
+                    print(e.message)
+                    print(e.label)
+                    print(e.method)
                     if e.message == "Bad Request: message is not modified":
                         update.message.delete()
                         await update.message.answer(text, reply_markup=kbd.markup)
